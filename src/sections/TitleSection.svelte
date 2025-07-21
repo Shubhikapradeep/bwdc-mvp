c37068bb5d4c009bc108673abbec828f32e72e43
Shubhikapradeep <pradeepshubhika@gmail.com>
2025-07-22 00:54:33 +0530

    Final MVP for deployment

--- src/sections/TitleSection.svelte
+++ src/sections/TitleSection.svelte
@@ -1,8 +1,8 @@
 <script>
     import TitleCard from "../lib/TitleCard.svelte";
     
-    const title = "Welcome to the KWK Scrollytelling Template";
-    const subtitle = "A journey through data, creativity, and code";
+    const title = "Credit, Collateral, and the  real Cost of Black Business Ownership";
+    const subtitle = "Exploring how factors like access to credit, loans, median incomes, assets and debts actually support increase in Black Business Ownership and thus Black Wealth";
 </script>
 
 <TitleCard {title} {subtitle} />
